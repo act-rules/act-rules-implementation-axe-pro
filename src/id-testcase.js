d7b91b9e308aea9fef8dcf8cefe3852114fad631
Wilco Fiers <wilco.fiers@deque.com>
2019-10-23 12:33:46 +0200

    feat: Add data for Links with identical accessible names

--- src/id-testcase.js
+++ src/id-testcase.js
@@ -1,7 +1,7 @@
 const assert = require('assert')
 
 module.exports = function identifyTestCase (fileName) {
-  const match = fileName.match(/testcases_([a-z0-9]{6})_([a-z0-9]+)\s/)
+  const match = fileName.match(/([a-z0-9]{6})_([a-z0-9]+)\s/)
   assert(Array.isArray(match), `Unable to determine rule and test case for '${fileName}'`)
   assert(match[2].length > 20, `Testcase ID fragement '${match[2]}' too far truncated`)
 
